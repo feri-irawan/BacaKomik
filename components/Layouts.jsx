30f1c64991974535d68a344ffd921ac66bf4e422
Feri Irawan <Feriirawans1998@gmail.com>
2022-04-15 17:26:04 +0700

    fix(ui): font

--- components/Layouts.jsx
+++ components/Layouts.jsx
@@ -10,6 +10,17 @@ export default function Layouts({ children, title }) {
       <Head>
         <title>{title ? `${appName} - ${title}` : appName}</title>
         <link rel="icon" href="/favicon.ico" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <Header />
