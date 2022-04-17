3819301d1586a4eb8190636a90694dc3feedd4a7
Feri Irawan <Feriirawans1998@gmail.com>
2022-04-17 15:10:01 +0700

    feat(api): multiple search

--- pages/api/comics/index.js
+++ pages/api/comics/index.js
@@ -1,7 +1,42 @@
 import { getComics } from '../../../utils/scraper'
 
 export default async function Comics(req, res) {
-  const { s, max_results } = req.query
+  const {
+    s,
+    title,
+    author,
+    year,
+    status,
+    order,
+    type,
+    genre,
+    multiple_search,
+    max_results
+  } = req.query
+
+  // Multiple searching
+  if (multiple_search === 'true') {
+    const multipleSearchResults = await getComics(
+      {
+        title: title ?? '',
+        author: author ?? '',
+        yearx: year ?? '',
+        status: status ?? '',
+        order: order ?? '',
+        type: type ?? '',
+        genre: genre ?? []
+      },
+      max_results
+    )
+    res.status(200).json({
+      code: 200,
+      message: `${multipleSearchResults.length} komik hasil pencarian`,
+      queries: req.query,
+      data: multipleSearchResults
+    })
+
+    return false
+  }
 
   if (s) {
     // Search
